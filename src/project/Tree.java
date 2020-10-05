53f65938b0dd4dbaa2d61d9371c7c832d4af635b
Daniel Aguiar <58986082+Danny-7@users.noreply.github.com>
2020-10-05 13:32:46 +0200

    advance

--- src/project/Tree.java
+++ src/project/Tree.java
@@ -5,44 +5,81 @@ import java.util.List;
 
 public class Tree {
     private Tree leftTree, rightTree;
-    private double value;
+    private List<BagObject> values;
     private int depth; // profondeur de l'arbre
     private double upperBound; // borne supérieure
     private double lowerBound; // borne inférieure
+    private double profit;
     private List<BagObject> bestPath; // solution finale
     
     public Tree() {}
     
-    public Tree(double value) {
+    public Tree(List<BagObject> items) {
     	this();
-    	this.value = value;
+    	this.values = new ArrayList<>();
     }
 
     public Tree(List<BagObject> items, double maxWeight, int index){
         if( index < items.size())
             throw new IllegalArgumentException("La profondeur de l'arbre est trop grande");
 
-        if(items.get(index).getValue() < this.value) {
-        	this.leftTree = new Tree(items.get(index).getValue());
-        	this.lowerBound = this.leftTree.getValue();
+        if(items.get(index).getValue() < this.upperBound) {
+        	List<BagObject> newValues = this.getSetOfValues();
+        	// add the item to the list
+        	newValues.add(items.get(index));
+        	// the leftTree takes the list of his parent with the object added
+        	this.leftTree = new Tree(newValues);
+        	this.leftTree.setUpperBound(this.upperBound);
+        	this.leftTree.setLowerBound(this.lowerBound);
+        	// the right tree takes the same list of his parent
+        	this.rightTree = new Tree(this.getSetOfValues());
         }
-        if(items.get(index).getValue() > this.value) {
-        	this.rightTree = new Tree(items.get(index).getValue());
-        	this.lowerBound = this.rightTree.getValue();
+        if(items.get(index).getValue() > this.upperBound) {
+        	this.rightTree = new Tree(this.getSetOfValues());
+        	this.rightTree.setLowerBound(this.lowerBound);
         }
         
         this.depth = ++index;
     }
     
-    public double getValue() {
-    	return this.value;
-    }
+    
+    // allows to get the values of a tree node
+    public List<BagObject> getSetOfValues(){
+    	return new ArrayList<BagObject>(values);
+   }
+    
+    
+   private double getBoundProfit() {
+	   return this.profit += this.rightTree.getBoundProfit();
+   }
+	   
+	   
+   // allows to get the sum of all values in the node
+   private double getSumListValues() {
+	   if(this.values.size() == 0)
+		   return 0.0;
+	   double value = 0.0;
+	   for(BagObject item :this.values) {
+		   value += item.getValue();
+	   }
+	   
+	   return value;
+   }
     
     public double getUpperBound(List<BagObject> items) {
-    	double value = this.value;
-    	for(BagObject item: items) {
-    		value += item.getValue();
+    	double value = this.getSumListValues();
+    	for(int i = depth; i< items.size(); ++i) {
+    		value += items.get(i).getValue();
     	}
     	return value; 
     }
+    
+    public void setLowerBound(double lowerBound) {
+    	this.lowerBound = lowerBound;
+    }
+    
+    public void setUpperBound(double upperBound) {
+    	this.upperBound = upperBound;
+    }
+    
 }
